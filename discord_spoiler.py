c37b71cdc0d105ff2824e8288acec82b8765a142
Michalis Ioak <ioakimidismichalis@gmail.com>
2020-11-30 18:00:05 +0200

    first commit

---
+++ discord_spoiler.py
@@ -0,0 +1,11 @@
+val = input("Enter your value: ") 
+array = [char for char in val]
+narray = ["||"]
+for i in range(len(val)):
+    narray += array[i] + "||||"
+narray.pop()
+narray.pop()
+str1 = ""
+for ele in narray:  
+        str1 += ele
+print(str1)
